a8c7306fee6530ed565ad415cd38b56eed4c7a8f
mtdanov <mtdanov@gmail.com>
2024-12-10 22:11:03 +0200

    add working-time component

---
+++ client/src/app/user/working-time/working-time.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-working-time',
+  standalone: true,
+  imports: [],
+  templateUrl: './working-time.component.html',
+  styleUrl: './working-time.component.css'
+})
+export class WorkingTimeComponent {
+
+}
