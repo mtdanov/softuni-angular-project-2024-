1b4e1d33c5fe2d8555fb3cdcd2f329d5948f8db8
mtdanov <mtdanov@gmail.com>
2024-12-11 20:55:45 +0200

    add

--- client/src/app/user/working-time/working-time.component.ts
+++ client/src/app/user/working-time/working-time.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-working-time',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, WorkingTimeComponent],
   templateUrl: './working-time.component.html',
-  styleUrl: './working-time.component.css'
+  styleUrls: ['./working-time.component.css'],
 })
 export class WorkingTimeComponent {
+  @Input() dayOfWeek!: string;
+  @Input() isOwner!: boolean;
+  @Input() time!: any;
+  @Output()
+  handleSubmit = new EventEmitter<{ day: string; time: string }>();
+  @Output() closed = new EventEmitter<void>();
 
+  workTimeForm = new FormGroup({
+    startTime: new FormControl('', [Validators.required]),
+    endTime: new FormControl('', [Validators.required]),
+  });
+  constructor(private fb: FormBuilder) {
+  }
+
+  close(): void {
+    this.closed.emit();
+  }
+  onSubmit() {
+    if (this.workTimeForm.valid) {
+      const time = `${this.workTimeForm.value.startTime}-${this.workTimeForm.value.endTime}`;
+      this.handleSubmit.emit({ day: this.dayOfWeek, time });
+    }
+    this.closed.emit();
+  }
 }
